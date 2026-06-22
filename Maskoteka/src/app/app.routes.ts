87db63a0981c48eac12cff506ba99a545b9d31e8
Josue Ccanto <josueccv26@gmail.com>
2026-06-21 21:27:26 -0500

    feat: add agendar

--- Maskoteka/src/app/app.routes.ts
+++ Maskoteka/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { PagoTarjeta } from './pages/pago-tarjeta/pago-tarjeta';
 import { AuthenticatedLayout } from './core/authenticated-layout';
+import { Agendar } from './pages/agendar/agendar';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -11,6 +12,7 @@ export const routes: Routes = [
     children: [
       { path: 'dashboard', component: Dashboard },
       { path: 'pago-tarjeta', component: PagoTarjeta },
+      { path: 'agendar', component: Agendar },
     ],
   },
   { path: '**', redirectTo: 'login' },
