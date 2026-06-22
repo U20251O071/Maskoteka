289e05500560849a3259c8b32a489f4713169c6b
Josue Ccanto <josueccv26@gmail.com>
2026-06-21 21:44:24 -0500

    feat: add agendar

--- Maskoteka/src/app/app.routes.ts
+++ Maskoteka/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ResumenCita } from './pages/resumen-cita/resumen-cita';
 import { PagoTarjeta } from './pages/pago-tarjeta/pago-tarjeta';
 import { PagoExitoso } from './pages/pago-exitoso/pago-exitoso';
 import { AuthenticatedLayout } from './core/authenticated-layout';
+import { Agendar } from './pages/agendar/agendar';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -15,6 +16,7 @@ export const routes: Routes = [
       { path: 'pago-tarjeta', component: PagoTarjeta },
       { path: 'pago-exitoso', component: PagoExitoso },
       { path: 'resumen-cita', component: ResumenCita },
+      { path: 'agendar', component: Agendar },
     ],
   },
   { path: '**', redirectTo: 'login' },
